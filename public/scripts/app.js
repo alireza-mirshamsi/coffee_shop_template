8d805ae02ea6de5fbd2b3f2c6e2223548f410934
alireza-mirshamsi <alireza.mirshamsi96@gmail.com>
2023-10-28 20:53:11 +0330

    update project

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,15 +5,18 @@ const navOpenBtn = document.querySelector(".nav-icon");
 const navCloseBtn = document.querySelector(".nav-close-btn");
 const nav = document.querySelector(".nav");
 const overlay = document.querySelector(".overlay");
+const cartOpenBtn = document.querySelector(".cart-icon");
+const cartCloseBtn = document.querySelector(".cart-close-btn");
+const cart = document.querySelector(".cart");
 
 toggleThemeBtns.forEach(btn => {
-    btn.addEventListener("click", function() {
-        if (localStorage.theme === "dark") {
+    btn.addEventListener("click", function (){
+        if (localStorage.theme === "dark"){
             document.documentElement.classList.remove("dark");
             localStorage.theme = "light";
         } else {
             document.documentElement.classList.add("dark");
-            localStorage.setItem("theme", "dark");
+            localStorage.setItem("theme" , "dark");
         }
     })
 })
@@ -21,19 +24,35 @@ submenuOpenBtn.addEventListener("click", (e) => {
     e.currentTarget.parentElement.classList.toggle("text-orange-300");
     submenu.classList.toggle("submenu--open");
 })
-navOpenBtn.addEventListener("click", () => {
-    nav.classList.remove("-right-64");
-    nav.classList.add("right-0");
-    overlay.classList.add("overlay--visible");
-
-})
-
 
 function closeNav() {
     nav.classList.remove("right-0");
     nav.classList.add("-right-64");
     overlay.classList.remove("overlay--visible");
 }
+function closeCart() {
+    cart.classList.remove("left-0");
+    cart.classList.add("-left-64");
+    overlay.classList.remove("overlay--visible");
+}
 
-navCloseBtn.addEventListener("click", closeNav)
-overlay.addEventListener("click", closeNav)
+
+navOpenBtn.addEventListener("click",() => {
+    nav.classList.remove("-right-64");
+    nav.classList.add("right-0");
+    overlay.classList.add("overlay--visible");
+    overlay.addEventListener("click", closeNav)
+})
+cartOpenBtn.addEventListener("click",() => {
+    cart.classList.remove("-left-64");
+    cart.classList.add("left-0");
+    overlay.classList.add("overlay--visible");
+    overlay.addEventListener("click", closeCart)
+})
+
+navCloseBtn.addEventListener("click", closeNav);
+cartCloseBtn.addEventListener("click", closeCart);
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -31,8 +31,21 @@ module.exports = {
             },
             spacing: {
                 "30": "7.5rem"
+            },
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: "1rem",
+                    lg: "0.625rem"
+                }
             }
         },
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+        }
     },
     plugins: [
         function({ addVariant }) {
